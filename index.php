118441bb3d455fed1189dce3ac1c19ffb2c1a7cd
dean.haines <dean.haines@freetimers.com>
2018-01-02 15:10:57 +0000

    index tweak

--- index.php
+++ index.php
@@ -2,17 +2,11 @@
 
 use Chippyash\Type\Number\FloatType;
 use Chippyash\Type\String\StringType;
-use Vbpupil\Audit\Audit;
 use Vbpupil\Calculate\CalculateCreditCardInterest;
 use Vbpupil\Card\CardType;
 use Vbpupil\Card\CreditCard;
-use Vbpupil\Card\DebitCard;
-use Vbpupil\Card\PaymentCard;
 
 include 'vendor/autoload.php';
-
-
-//echo (Audit::AUDIT_MSG['debitAccount']);
 
 $card = new CreditCard();
 
@@ -33,15 +27,3 @@ $card
 ->calculateInterest( new CalculateCreditCardInterest());
 
 dump($card);
-
-//$debitCard = new DebitCard();
-//
-//$debitCard
-//    ->setCardName(new StringType('Debit Card'))
-//    ->setCardType(new CardType('debit'))
-//    ->setBalance(new FloatType(100.00))
-//    ->setOwner(new StringType('Mr D Haines'))
-//    ->setValidDate(new DateTime('1/12/17', new DateTimeZone('Europe/London')))
-//    ->setExpDate(new DateTime('01/12/18', new DateTimeZone('Europe/London')));
-//
-//dump($debitCard);
